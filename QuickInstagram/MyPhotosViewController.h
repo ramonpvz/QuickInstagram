7f21a1bea9820293fdc67efa1dad96d47e2f70bc
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-10-28 16:59:56 -0600

    add MyPhotosViewController

---
+++ QuickInstagram/MyPhotosViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyPhotosViewController.h
+//  QuickInstagram
+//
+//  Created by Eduardo Alvarado Díaz on 10/28/14.
+//  Copyright (c) 2014 globant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyPhotosViewController : UIViewController
+
+@end
